14b2f31feaba7960922e2e8d7f74c80475a19fe4
Hamzah Jamal Eddin <hamzahjeddin@gmail.com>
2023-05-01 10:40:27 -0400

    horizontal bar added

--- app.js
+++ app.js
@@ -7,7 +7,5 @@
             document.getElementById(button.dataset.id).classList.add("active");
         })
     });
-    document.querySelector(".theme-btn").addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
-    })
+
 })();
